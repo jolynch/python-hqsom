4bc42220b2b1d6b9833b215273118944e9ff9c24
Joseph Lynch <jolynch@mit.edu>
2012-12-12 15:12:23 -0500

    Better commenting of som.py
    The basic library implementation is in som, so I took some time to
    clean it up some so that people can understand it.

--- som.py
+++ som.py
@@ -6,24 +6,49 @@ import random
 # EG: We want to classify 5d vectors and we want the SOM to output a 4d activation vector
 #  => SOM(5,4)
 class SOM(object):
-    
-    #TODO: Bottom of page 4, luttrell's method needed to expand size
-    
+    """
+    Self Organizing Map
+
+    Implementation of a spatial clustering algorithm whereby there are
+    inputs of degree input_size that are spatially clustered into the
+    output space of degree output_size.  This means that vectors that are
+    close to each other in input space should result in near identical
+    output vectors.
+
+    TODO:
+        - Bottom of page 4, luttrell's method needed to expand size
+          of map size depending on usage
+    """
+
     def __init__(self, input_size, output_size, pure=False):
-        #random_array = [random.random() for i in range(input_size*output_size)]
-        #random_array2 = [random.random() for i in range(input_size*output_size)]
-        #random_array = np.array(random_array) + np.array(random_array2)[::-1]
-        #random_array = random_array/2.0
-        
-        #self.units = np.array(random_array).reshape((output_size, input_size))
+        """
+        Initialize the data structures required for the SOM of input input_size
+        and output of output_size
+
+        Members:
+            units - A numpy matrix of dimension input_size x output_size
+            mse_ema - An exponential moving average of the MSE between the
+                      BMU and the input data.
+            pure - A flag to indicate whether the paper's implementation
+                   should be used or if our improvements should be used
+            time - How many timesteps have passed
+        """
         self.units = np.random.random((output_size, input_size))
         self.mse_ema = 1e-20
-        #If we consider this a non-pure SOM, use our "improvements"
         self.pure = pure
         self.time = 0
 
-    #Best matching unit, returns the index of the bmu
     def bmu(self, unit_input):
+        """
+        Calculate and return the best matching unit, which is the concept vector
+        closest to the unit input vector
+
+        Args:
+            unit_input - The input data to examine
+
+        Returns the *index* of the best matching unit in the mapspace
+        """
+
         distances = [np.linalg.norm(unit - unit_input) for unit in self.units]
         return np.argmin(distances)
 
@@ -31,9 +56,30 @@ class SOM(object):
     # This basically tells us how close we are to the input and bmu
     # NOTE: Currently we use a gaussian, a mexican hat function might be better
     def nb_func(self, unit_i, unit_bmu, mse_bmu, unit_input, spread):
-        #What do we want to consider 0?
+        """
+        Calculate the adaptive neighborhood function h_ib(t), which is a measure
+        of how close the provided map unit is to the provided input, relative to
+        the best matching unit.
+
+        The basic idea is h_ib(t) = exp(-(unit_i, unit_bmu) /
+                                        (mse(unit_input, unit_bmu)*spread))
+
+        although in our improved version we use a mexican hat function
+        instead of a gaussian
+
+        Args:
+            unit_i - The map unit *index* to compare to the input and the bmu
+            unit_bmu - The map unit *index* of the bmu
+            mse_bmu - The MSE of the bmu and the input (computed once and passed in)
+            unit_input - The input unit
+            spread - The spread parameter that decided how wide the tails are
+
+        Returns val: The value of the neighborhood function
+
+        """
+        # For numerical stability we need to pick something that is effectively zero
+        # and not actually do the computation for various very wonky values
         eff_zero = 1e-20
-        #e^(-oo) = 0
         if mse_bmu < eff_zero:
             return eff_zero
         if self.pure:
@@ -43,16 +89,21 @@ class SOM(object):
             val = max((1-t/spread)*np.exp(- t / (mse_bmu * spread)), eff_zero)
         return val
 
-    #SOM update rule
-    # unit_input : An input vector of size N
-    # rate : (gamma) the learning rate
-    # spread : (sigma) the variance of the neighborhood function
-    # 
-    # w_i(t1) = w_i(t) + rate * h_ib(t)*(x(t)-w_i(t))
-
-    #TODO: select spread based on variance as per the paper
 
     def update(self, unit_input, rate, spread):
+        """
+        SOM update rule
+
+        TODO: select spread based on variance as per the paper
+
+        Args:
+            unit_input - An input vector of size N
+            rate - (gamma) the learning rate
+            spread - (sigma) the variance of the neighborhood function
+
+        For eeach w_i in the map space, apply the following update rule
+            w_i(t1) = w_i(t) + rate * h_ib(t)*(x(t)-w_i(t))
+        """
         if len(unit_input) != self.units.shape[1]:
             print "Input of length {0} incompatible with SOM length {1}".format(len(unit_input),self.units.shape[1])
             return
@@ -62,22 +113,21 @@ class SOM(object):
 
         for weight_index in range(len(self.units)):
             w_t = self.units[weight_index]
-
             self.units[weight_index] = w_t + rate*self.nb_func(weight_index, bmu_index, mse, unit_input, spread)*(unit_input-w_t)
-       
-        #print "MSE: {}".format(mse)
-        #print "EMA: {}".format(self.mse_ema)
+
         if not self.pure:
             if mse / self.mse_ema > 10:
                 self.t = 0
                 print "Unexpectedly Large Miss!! Stealing BMU {}".format(bmu_index)
-                print "Used effective sigma {} for update".format(mse * spread)
+                print "Used effective sigma {} for update".format(.5)
                 self.units[bmu_index] = self.units[bmu_index] + .5*(unit_input - self.units[bmu_index])
             self.mse_ema = max(.9*(self.mse_ema) + .1*(mse), 1e-20)
 
-
-
     def mse(self, unit_input, som_unit=None):
+        """
+        Calculate the Mean Squared Error between unit_input
+        and som_unit
+        """
         bmu = None
         if som_unit != None:
             bmu = som_unit
@@ -106,12 +156,15 @@ class SOM(object):
             val[bmu_index] = 1
         return val
 
-# Recursive SOM, basically makes the update rule time dependent
-# Should theoretically capture time dependencies
 class RSOM(SOM):
-    
-    #Major difference to a SOM is that we have a first difference matrix as well
-    # as the standard units matrix
+    """
+    Recursive SOM, basically makes the update rule time dependent
+    This should theoretically capture time dependencies
+
+    Major difference to a SOM is that we have a first difference matrix as well
+    as the standard units matrix
+    """
+
     def __init__(self, input_size, output_size, pure=False):
         super(RSOM, self).__init__(input_size, output_size, pure)
         self.differences = np.zeros((output_size, input_size))
@@ -119,21 +172,25 @@ class RSOM(SOM):
     def bmu_r(self, unit_input):
         distances = [np.linalg.norm(diff) for diff in self.differences]
         return np.argmin(distances)
-    
-    #So that we don't have to flush the RSOM with 0s all the damn time
+
     def reset(self):
+        """
+        So that we don't have to flush the RSOM with 0s all the damn time
+        """
         self.differences = np.zeros(self.differences.shape)
         r_i = np.random.randint(len(self.differences))
         self.differences[r_i] = .001
-    
-    #RSOM update rule
-    # Parameters same as SOM update rule except for time_decay
-    # time_decay : (alpha) the relative weight we give to past samples vs current samples
-    # 
-    #  y_i(t1) = (1-alpha)*y_i(t)+alpha*(x(t)-w_i(t))
-    #  w_i(t1) = w_i(t) + rate * h_ibr(t)*y(t)
 
     def update(self, unit_input, rate, spread, time_decay):
+        """
+        RSOM update rule
+          Parameters same as SOM update rule except for time_decay
+          time_decay : (alpha) the relative weight we give to past samples vs current samples
+
+        y_i(t1) = (1-alpha)*y_i(t)+alpha*(x(t)-w_i(t))
+        w_i(t1) = w_i(t) + rate * h_ibr(t)*y(t)
+        """
+
         if len(unit_input) != self.units.shape[1]:
             print "Input of length {0} incompatible with SOM length {1}".format(len(unit_input),self.units.shape[1])
             return
@@ -148,11 +205,5 @@ class RSOM(SOM):
             self.differences[weight_index] = (1-time_decay)*self.differences[weight_index] + time_decay * (unit_input-w_t)
         if not self.pure:
             rate = 2 * rate * np.exp(-np.power(self.time, 1/16.0))
-            self.time += 1 
-        #if not self.pure:
-            #if mse / self.mse_ema > 10:
-                #print "Massive Miss!! Stealing BMU {}".format(bmu_r_index)
-                #print "Used effective sigma {} for update".format(mse * spread)
-                #self.units[bmu_r_index] = self.units[bmu_r_index] + .5*(unit_input - self.units[bmu_r_index])
-            #self.mse_ema = max(.9*(self.mse_ema) + .1*(mse), 1e-20)
+            self.time += 1
 
